2e2030d748a21f7d6a609d70d47f8dfb838290d8
Reza <Reza@DESKTOP-CKL538G>
2024-01-17 23:08:02 +0330

    test websocket

---
+++ Blazor/Websocket/Services/Stock.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Net.WebSockets;
+using System.Text.Json;
+
+namespace Websocket.Services;
+
+public class Stock
+{
+    private HubConnection connection;
+
+    public Stock()
+    {
+        
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("wss://ws.finnhub.io?token=cmjrgapr01qnr60h1hs0cmjrgapr01qnr60h1hsg")
+            .Build();
+    }
+
+    public async void Send()
+    {
+        //var test = await connection
+        //    .SendAsync();
+
+        var uri = new Uri("wss://ws.finnhub.io?token=cmjrgapr01qnr60h1hs0cmjrgapr01qnr60h1hsg");
+
+        var aa = new ClientWebSocket();
+        await aa
+            .ConnectAsync(uri,
+            CancellationToken.None);
+
+        var obj = new
+        {
+            type = "subscribe",
+            symbol = "BINANCE:BTCUSDT"
+        };
+
+        // System.Text.Json
+        string jsonStringSystem = JsonSerializer.Serialize(obj);
+        var result = await aa.SendAsync(, WebSocketMessageType.Text, false, CancellationToken.None);
+    }
+}
