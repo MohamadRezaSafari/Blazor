199b730ac3cc1d91701b1beca83ce2f92648217c
Reza <Reza@DESKTOP-CKL538G>
2024-01-18 21:20:25 +0330

    Stock market Initial

---
+++ Blazor/Websocket/Services/Dto/ResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Websocket.Services.Dto;
+
+public class ResponseDto
+{
+    [JsonPropertyName("data")]
+    public IReadOnlyList<Data> Data { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Data
+{
+    [JsonPropertyName("s")]
+    public string Symbol { get; set; }
+
+    [JsonPropertyName("p")]
+    public decimal LastPrice { get; set; }
+
+    [JsonPropertyName("t")]
+    public long UNIXTimestamp { get; set; }
+
+    [JsonPropertyName("v")]
+    public decimal Volume { get; set; }
+}
--- Blazor/Websocket/Services/Stock.cs
+++ Blazor/Websocket/Services/Stock.cs
@@ -1,42 +1,53 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
-using System.Net.WebSockets;
+﻿using System.Net.WebSockets;
+using System.Text;
 using System.Text.Json;
+using Websocket.Services.Dto;
 
 namespace Websocket.Services;
 
 public class Stock
 {
-    private HubConnection connection;
+    private readonly Uri uri =
+        new Uri("wss://ws.finnhub.io?token=cmjrgapr01qnr60h1hs0cmjrgapr01qnr60h1hsg");
+    private ClientWebSocket webSocket;
 
-    public Stock()
+    public async Task ConnectAsync()
     {
-        
-
-        connection = new HubConnectionBuilder()
-            .WithUrl("wss://ws.finnhub.io?token=cmjrgapr01qnr60h1hs0cmjrgapr01qnr60h1hsg")
-            .Build();
+        webSocket = new ClientWebSocket();
+        await webSocket.ConnectAsync(uri, CancellationToken.None);
     }
 
-    public async void Send()
+    public async IAsyncEnumerable<string> SendAsync()
     {
-        //var test = await connection
-        //    .SendAsync();
-
-        var uri = new Uri("wss://ws.finnhub.io?token=cmjrgapr01qnr60h1hs0cmjrgapr01qnr60h1hsg");
-
-        var aa = new ClientWebSocket();
-        await aa
-            .ConnectAsync(uri,
-            CancellationToken.None);
-
         var obj = new
         {
             type = "subscribe",
             symbol = "BINANCE:BTCUSDT"
         };
+        string sendMessage = JsonSerializer.Serialize(obj);
+        ArraySegment<byte> sendBuffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(sendMessage));
+        await webSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
 
-        // System.Text.Json
-        string jsonStringSystem = JsonSerializer.Serialize(obj);
-        var result = await aa.SendAsync(, WebSocketMessageType.Text, false, CancellationToken.None);
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result;
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+            do
+            {
+                result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                string message = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                Console.WriteLine("Received message: " + message);
+                yield return message;
+            }
+            while (!result.EndOfMessage);
+        }
+
+        yield return String.Empty;
+    }
+
+    public async Task<ResponseDto> DeserializeResponse(string message)
+    {
+        var response = JsonSerializer.Deserialize<ResponseDto>(message);
+        return await Task.FromResult(response);
     }
 }
